065b0d72982c1c3401a88764386d80785fe4bc60
Abhushan Chaulagain <abushanchaulagain@gmail.com>
2026-01-03 15:53:11 +0545

    add B

--- Cbasics/hello.c
+++ Cbasics/hello.c
@@ -4,7 +4,7 @@ int main(void)
 {
    int age = 25;
    float height = 5.9;
-   char grade = 'A';
+   char grade = 'B';
    printf("age: %d, height; %.1f, Grade; %c\n ", age, height, grade );
    return 0;
   
